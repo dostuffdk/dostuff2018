4f1a618f2ec6a03f98be48d76c054eec5890751f
Anders Mogensen <anders@dostuff.dk>
2018-09-28 20:36:07 +0200

    Blank slate.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 import '@babel/polyfill';
 import './css/main.css';
-
-const startNode = document.createElement('h1');
-startNode.innerHTML = 'Anders Mogensen';
-document.body.appendChild(startNode);
